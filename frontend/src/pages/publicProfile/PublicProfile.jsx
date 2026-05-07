3bd828f464e7fae77e1c41cc9e9311c48b43a5a7
Infiniper <infiniper@gmail.com>
2026-05-07 08:17:21 +0530

    Created publicprofile and enhanced PDP page

--- frontend/src/pages/publicProfile/PublicProfile.jsx
+++ frontend/src/pages/publicProfile/PublicProfile.jsx
@@ -1,4 +1,4 @@
-// frontend/src/pages/publicProfile/PublicProfile.jsx
+// frontend/src/pages/publicProfile/PublicProfile.jsx 
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import styles from "../dashboard/Dashboard.module.css";
