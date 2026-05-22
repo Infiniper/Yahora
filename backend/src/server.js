7a811c644276a478ce716b53b2ab4c16580505a4
Infiniper <infiniper@gmail.com>
2026-05-22 09:11:38 +0530

    Added bot in demo mode to reply for messages

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
+// backend\src\server.js
 import app from './app.js';
 import dotenv from 'dotenv';
+import { startCronJobs } from './utils/cronJobs.js';
 
 // Load env variables from the backend root
 dotenv.config(); 
@@ -8,4 +10,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Backend server is running on http://localhost:${PORT}`);
+
+    startCronJobs();
 });
